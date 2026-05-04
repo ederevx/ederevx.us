03543db9775ec831a1bfea172612b298731fa5ef
Edrick Sinsuan <evcsinsuan@gmail.com>
2026-05-04 15:34:02 -0400

    resources/js/pages: BaseLayout: Define CSP configuration based on environment
    We need to make sure the CSP configuration matches the environment
    the website is running in to make it effective.
    
    Signed-off-by: Edrick Sinsuan <evcsinsuan@gmail.com>

--- resources/js/pages/common/layout.tsx
+++ resources/js/pages/common/layout.tsx
@@ -151,6 +151,25 @@ export function BaseLayout({
     children?: React.ReactNode,
     metaProps?: {name: string, content: string}[],
 }) {
+    // Implement CSP as strict as possible depending on app environment
+    const CSPconfig = () => {
+        if (import.meta.env.VITE_APP_ENV === 'production') {
+            return cn("default-src 'none';", 
+                "script-src 'self'",
+                "connect-src 'self'",
+                "img-src 'self';",
+                "style-src 'self' 'unsafe-inline' https://fonts.bunny.net;",
+                "font-src 'self' https://fonts.bunny.net;");
+        } else {
+            return cn("default-src 'none';",
+                "script-src 'self' 127.0.0.1:*;",
+                "connect-src 'self' ws://127.0.0.1:*;",
+                "img-src 'self';",
+                "style-src 'self' 'unsafe-inline' https://fonts.bunny.net;",
+                "font-src 'self' https://fonts.bunny.net 127.0.0.1:*;");
+        }
+    };
+
     // Handle dark class on root DOM in BaseLayout
     React.useEffect(() => {
         const windowDark = window.matchMedia('(prefers-color-scheme: dark)');
@@ -173,14 +192,7 @@ export function BaseLayout({
     return (
         <>
             <Head title={title}>
-                {/* Implement CSP as strict as possible */}
-                <meta http-equiv="Content-Security-Policy"
-                    content="default-src 'none'; 
-                            script-src 'self' 127.0.0.1:5173; 
-                            connect-src 'self' ws://127.0.0.1:5173; 
-                            img-src 'self'; 
-                            style-src 'self' 'unsafe-inline';
-                            font-src 'self' https://fonts.bunny.net 127.0.0.1:5173;"/>
+                <meta http-equiv="Content-Security-Policy" content={CSPconfig()} />
                 {metaProps?.map((prop) => 
                     <meta name={prop.name} content={prop.content} />
                 )}
