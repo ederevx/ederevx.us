4fa887d57a303dce99503136f9dc30ac4660a1cf
Edrick Sinsuan <evcsinsuan@gmail.com>
2026-05-04 21:05:15 -0400

    bootstrap: Add missing AddCspHeaders
    Woops, I thought it was automatically setup by the class already.
    Implement it properly in boostrap/app per Spatie's README.
    
    Signed-off-by: Edrick Sinsuan <evcsinsuan@gmail.com>

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,6 +6,8 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
 
+use Spatie\Csp\AddCspHeaders;
+
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
@@ -16,6 +18,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->web(append: [
             HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
+            AddCspHeaders::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
