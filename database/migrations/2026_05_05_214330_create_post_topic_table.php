1d379605ed63c251afb58885289f6796208e92de
Edrick Sinsuan <evcsinsuan@gmail.com>
2026-05-05 16:20:06 -0400

    database: Make post and topic models, migration, and factory
    Implemented based on:
    https://franklinstrube.com/blog/how-to-laravel-blog-database-stuff
    
    Huge thanks to Franklin P. Strube!
    
    Signed-off-by: Edrick Sinsuan <evcsinsuan@gmail.com>

---
+++ database/migrations/2026_05_05_214330_create_post_topic_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        // Follow Eloquent ORM for many-to-many relationships
+        Schema::create('post_topic', function (Blueprint $table) {
+            $table->foreignId('post_id')->references('id')->on('posts')->cascadeOnDelete();
+            $table->foreignId('topic_id')->references('id')->on('topics')->cascadeOnDelete();
+            $table->primary(['post_id', 'topic_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('post_topic');
+    }
+};
